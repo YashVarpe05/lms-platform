834d7e81a7f945b1487e87dabcbbd4c034055334
YashVarpe05 <yashvarpe2005@gmail.com>
2025-08-07 23:21:04 +0530

    feat(auth): implement email OTP login and enhance session management

--- lib/env.ts
+++ lib/env.ts
@@ -4,10 +4,11 @@ import { z } from "zod";
 export const env = createEnv({
 	server: {
 		DATABASE_URL: z.string().url(),
-        BETTER_AUTH_SECRET: z.string().min(1),
-        BETTER_AUTH_URL: z.string().url(),
-        AUTH_GITHUB_CLIENT_ID: z.string().min(1),
-        AUTH_GITHUB_SECRET: z.string().min(1),
+		BETTER_AUTH_SECRET: z.string().min(1),
+		BETTER_AUTH_URL: z.string().url(),
+		AUTH_GITHUB_CLIENT_ID: z.string().min(1),
+		AUTH_GITHUB_SECRET: z.string().min(1),
+		RESEND_API_KEY: z.string().min(1),
 	},
 
 	// If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
