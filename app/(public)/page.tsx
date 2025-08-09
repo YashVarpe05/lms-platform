37ef5c6b856b62cf07f75118c25342e489deb9ef
YashVarpe05 <yashvarpe2005@gmail.com>
2025-08-09 23:38:11 +0530

    feat(auth): integrate Arcjet for enhanced signup protection and bot detection feat(env): add ARCJET_KEY to environment variables feat(deps): update package.json and pnpm-lock.yaml with Arcjet dependencies refactor(home): create a new Home component with enhanced UI and authentication handling

---
+++ app/(public)/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { Badge } from "@/components/ui/badge";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/themeToggle";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+const features = [
+	{
+		title: "Comprehensive Course",
+		description:
+            "Access a wide range of carefully curated designed by industry experts.",
+        icon:"📖"
+	},
+];
+
+export default function Home() {
+	const router = useRouter();
+	const { data: session } = authClient.useSession();
+	async function signOut() {
+		await authClient.signOut({
+			fetchOptions: {
+				onSuccess: () => {
+					router.push("/");
+					toast.success("Successfully signed out!");
+				},
+			},
+		});
+	}
+	return (
+		<>
+			<section className="relative py-20">
+				<div className="flex flex-col items-center text-center space-y-8">
+					<Badge variant="outline">The Future of Online Education</Badge>
+					<h1 className="text-4xl md:text-5xl font-bold tracking-tight ">
+						Elevate your Learning Experience
+					</h1>
+					<p className="max-w-[700px] text-muted-foreground md:text-xl ">
+						Discover a new way to learn with our modern, interactive learning
+						management system. Access high-quality resources and tools to
+						enhance your educational journey.
+					</p>
+
+					<div className="flex flex-col sm:flex-row gap-4 mt-8">
+						<Link href="/courses" className={buttonVariants({ size: "lg" })}>
+							Explore Courses
+						</Link>
+						<Link href="/login" className={buttonVariants({ size: "lg" })}>
+							Sign in
+						</Link>
+					</div>
+				</div>
+			</section>
+			<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"></section>
+		</>
+	);
+}
