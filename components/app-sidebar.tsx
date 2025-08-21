77e144833701d037c22b38ddb734ec413ef3cadb
YashVarpe05 <yashvarpe2005@gmail.com>
2025-08-21 18:54:51 +0530

    refactor(sidebar): remove NavDocuments component and update navigation links in AppSidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -5,21 +5,17 @@ import {
 	IconCamera,
 	IconChartBar,
 	IconDashboard,
-	IconDatabase,
 	IconFileAi,
 	IconFileDescription,
-	IconFileWord,
 	IconFolder,
 	IconHelp,
-	IconInnerShadowTop,
 	IconListDetails,
-	IconReport,
 	IconSearch,
 	IconSettings,
 	IconUsers,
 } from "@tabler/icons-react";
 
-import { NavDocuments } from "@/components/nav-documents";
+// import { NavDocuments } from "@/components/nav-documents";
 import { NavMain } from "@/components/nav-main";
 import { NavSecondary } from "@/components/nav-secondary";
 import { NavUser } from "@/components/nav-user";
@@ -44,12 +40,12 @@ const data = {
 	navMain: [
 		{
 			title: "Dashboard",
-			url: "#",
+			url: "/admin",
 			icon: IconDashboard,
 		},
 		{
-			title: "Lifecycle",
-			url: "#",
+			title: "Courses",
+			url: "/admin/courses",
 			icon: IconListDetails,
 		},
 		{
@@ -133,23 +129,6 @@ const data = {
 			icon: IconSearch,
 		},
 	],
-	documents: [
-		{
-			name: "Data Library",
-			url: "#",
-			icon: IconDatabase,
-		},
-		{
-			name: "Reports",
-			url: "#",
-			icon: IconReport,
-		},
-		{
-			name: "Word Assistant",
-			url: "#",
-			icon: IconFileWord,
-		},
-	],
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
@@ -173,7 +152,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 			</SidebarHeader>
 			<SidebarContent>
 				<NavMain items={data.navMain} />
-				<NavDocuments items={data.documents} />
+				{/* <NavDocuments items={data.documents} /> */}
 				<NavSecondary items={data.navSecondary} className="mt-auto" />
 			</SidebarContent>
 			<SidebarFooter>
