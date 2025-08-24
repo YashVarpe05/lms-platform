0c04b8499c3e374f849770a6d6e10a4650da84a8
YashVarpe05 <yashvarpe2005@gmail.com>
2025-08-25 01:58:03 +0530

    feat(courses): implement course creation page and validation schema

---
+++ lib/zodSchemas.ts
@@ -0,0 +1,43 @@
+import { categories } from "@arcjet/next";
+import { FileKey } from "lucide-react";
+import { Princess_Sofia } from "next/font/google";
+import z, { file } from "zod";
+
+export const courseLevels = ["Beginner", "Intermediate", "Advanced"] as const;
+export const courseStatus = ["Draft", "Published", "Archived"] as const;
+export const courseSchema = z.object({
+	title: z
+		.string()
+		.min(3, { message: "Title Must be at least 3 characters long " })
+		.max(100, { message: "Title Must be at least 100 characters long" }),
+	description: z
+		.string()
+		.min(3, { message: "Description Must be at least 3 characters long" })
+		.max(2555, {
+			message: "Description Must be at least 2555 characters long",
+		}),
+	fileKey: z
+		.string()
+		.min(1, { message: "File Key Must be at least 1 character long" }),
+	price: z.coerce.number().min(1, { message: "Price Must be at least 1" }),
+	duration: z.coerce
+		.number()
+		.min(1, { message: "Duration Must be at least 1" })
+		.max(500, { message: "Duration Must be at most 500" }),
+	level: z.enum(courseLevels, {
+		message: "Level Must be one of Beginner, Intermediate, Advanced",
+	}),
+	categories: z.string(),
+	smallDescription: z
+		.string()
+		.min(3, { message: "Small Description Must be at least 3 characters long" })
+		.max(200, {
+			message: "Small Description Must be at most 200 characters long",
+		}),
+	slug: z
+		.string()
+		.min(3, { message: "Slug Must be at least 3 characters long" }),
+	status: z.enum(courseStatus, {
+		message: "Status Must be one of: " + courseStatus.join(", "),
+	}),
+});
